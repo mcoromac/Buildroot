8630172603c62ad11b33e208d36c1608b6c7c870
mariano <mcoromac@stsa.info>
2018-05-15 10:40:10 -0600

    FINGERPRINT: Modified pyfprint package and added fingerprint python script

--- package/python-pyfprint/python-pyfprint.mk
+++ package/python-pyfprint/python-pyfprint.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 PYTHON_PYFPRINT = 1.0
-PYTHON_PYFPRINT_VERSION = b9f67b8531989d7d861687e037ba1b73a84f9067
-PYTHON_PYFPRINT_SITE = $(call github,luksan,pyfprint,$(PYTHON_PYFPRINT_VERSION))
-PYTHON_PYFPRINT_DEPENDENCIES = libfprint
+PYTHON_PYFPRINT_VERSION = tab6
+PYTHON_PYFPRINT_SITE = $(call github,mcoromac,pyfprint-cffi,$(PYTHON_PYFPRINT_VERSION))
+PYTHON_PYFPRINT_DEPENDENCIES = libfprint python-cffi python-pillow
 PYTHON_PYFPRINT_SETUP_TYPE = distutils
 
 $(eval $(python-package))
