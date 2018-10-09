7bcda405434c1542176fd33191f1b98a5583f9b4
mariano <mcoromac@stsa.info>
2018-10-09 11:20:31 -0600

    C-PERIPHERY: Added post install hooks for userspaceio compatibility.

--- package/c-periphery/c-periphery.mk
+++ package/c-periphery/c-periphery.mk
@@ -8,7 +8,7 @@ C_PERIPHERY_VERSION = v1.1.1
 C_PERIPHERY_SITE = $(call github,vsergeev,c-periphery,$(C_PERIPHERY_VERSION))
 C_PERIPHERY_INSTALL_STAGING = YES
 # only a static library
-C_PERIPHERY_INSTALL_TARGET = NO
+C_PERIPHERY_INSTALL_TARGET = YES
 C_PERIPHERY_LICENSE = MIT
 C_PERIPHERY_LICENSE_FILES = LICENSE
 
@@ -24,4 +24,12 @@ define C_PERIPHERY_INSTALL_STAGING_CMDS
 	cp -dpfr $(@D)/src/*.h $(STAGING_DIR)/usr/include/c-periphery/
 endef
 
+define SAVE_OBJ_AS_SHARED_LIBRARY
+	$(HOST_DIR)/bin/arm-linux-gnueabihf-ld -shared $(@D)/obj/i2c.o -o libperipheryi2c.so
+	ld -shared $(@D)/obj/spi.o -o libperipheryspi.so
+	ld -shared $(@D)/obj/serial.o -o libperipheryserial.so
+	ld -shared $(@D)/obj/mmio.o -o libperipherymmio.so
+endef
+ 
+C_PERIPHERY_POST_INSTALL_TARGET_HOOKS += SAVE_OBJ_AS_SHARED_LIBRARY
 $(eval $(generic-package))
